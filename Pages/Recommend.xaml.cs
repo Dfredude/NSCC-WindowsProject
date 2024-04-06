a85ded364cb0b748594e692e180c4f48bb8edcb0
Kanishk Dogra <kanishkmain@gmail.com>
2024-04-06 16:14:12 -0400

    Recommend Page

--- Pages/Recommend.xaml.cs
+++ Pages/Recommend.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,20 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MovieRecommend
+namespace MovieRecommend.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Recommend.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Recommend : Page
     {
-        public MainWindow()
+        public Recommend()
         {
             InitializeComponent();
-        }
-        private void Recommend_Click(object sender, RoutedEventArgs e)
-        {
-            mainFrame.NavigationService.Navigate(new Pages.Recommend());
         }
     }
 }
